34537bb6a189db9919f84cf55a959bbe908d651f
Seaborn Lee <seabornlee.cn@gmail.com>
2020-10-21 14:13:10 +0800

    refactor: parameterized test

--- src/test/java/commands/CommandParserTest.java
+++ src/test/java/commands/CommandParserTest.java
@@ -1,24 +1,39 @@
 package commands;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(Parameterized.class)
 public class CommandParserTest {
 
-    @Test
-    public void parse_前进一步() {
-        List<Command> commands = CommandParser.parse("F");
-        assertThat(commands).hasSize(1);
-        assertThat(commands.get(0)).isInstanceOf(MoveForwardCommand.class);
+    private String commandInString;
+    private Class<MoveForwardCommand> classOfCommand;
+
+    public CommandParserTest(String commandInString, Class<MoveForwardCommand> classOfCommand) {
+        this.commandInString = commandInString;
+        this.classOfCommand = classOfCommand;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"F", MoveForwardCommand.class},
+                {"B", MoveBackCommand.class}
+        });
     }
 
     @Test
-    public void parse_后退一步() {
-        List<Command> commands = CommandParser.parse("B");
+    public void 解析字符串到命令对象() {
+        List<Command> commands = CommandParser.parse(commandInString);
         assertThat(commands).hasSize(1);
-        assertThat(commands.get(0)).isInstanceOf(MoveBackCommand.class);
+        assertThat(commands.get(0)).isInstanceOf(classOfCommand);
     }
+
 }
